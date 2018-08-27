af88b989d39c793213f56efc4e0a34769e355862
rink72 <beardmans72@gmail.com>
2018-08-26 22:03:13 -0700

    Working on testing

--- controller.ps1
+++ controller.ps1
@@ -1,3 +1,22 @@
+# Module imports
+Import-Module psyaml
+
+
+# Deployment definition. This is a definition in YAML syntax.
+[Parameter(String)]
+[string]
+$DeploymentDefinition
+
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+$structuredDefinition = $DeploymentDefinition | ConvertFrom-Yaml
+
+$definitionName = $structuredDefinition.Name
+$virtualMachines = $structuredDefinition.VirtualMachines
+
+
+Write-Verbose -Message "Definition Name: $definitionName"
+Write-Verbose -Message "Virtual Machines:"
+$virtualMachines | Format-Table
 
