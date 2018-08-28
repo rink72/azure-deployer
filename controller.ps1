eb2819a216fcba9042ba324614e63ed492105f5c
rink72 <beardmans72@gmail.com>
2018-08-27 21:08:59 -0700

    Adding dummy yaml

--- controller.ps1
+++ controller.ps1
@@ -1,22 +1,38 @@
+
+Param
+(
+    # Deployment definition. This is a definition in YAML syntax.
+    [Parameter()]
+    [String] $DeploymentDefinition = @'
+---
+name: Dummy Deployment
+action: Create
+
+VirtualMachines:
+ - name: vm1
+   size: BIG
+ - name: vm2
+   size: smaller
+   network: private
+'@
+)
+
+
+
 # Module imports
 Import-Module psyaml
 
-
-# Deployment definition. This is a definition in YAML syntax.
-[Parameter(String)]
-[string]
-$DeploymentDefinition
-
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection
-Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+$null = Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
 
 $structuredDefinition = $DeploymentDefinition | ConvertFrom-Yaml
 
 $definitionName = $structuredDefinition.Name
 $virtualMachines = $structuredDefinition.VirtualMachines
+$action = $structuredDefinition.action
 
 
 Write-Verbose -Message "Definition Name: $definitionName"
+Write-Verbose -Message "Action: $action"
 Write-Verbose -Message "Virtual Machines:"
 $virtualMachines | Format-Table
-
