8e729000d2ef7d94d5663e637b241049ec566002
rink72 <beardmans72@gmail.com>
2018-08-29 23:30:59 -0700

    Fix var name

--- controller.ps1
+++ controller.ps1
@@ -33,8 +33,8 @@ if ( -not $WebhookData)
 }
 
 # Allow testing from the test pane
-if ( -not $WebhookData.RequestData ) { $deploymentDefinition = $WebhookData }
-else { $deploymentDefinition = $WebhookData.RequestData }
+if ( -not $WebhookData.RequestBody ) { $deploymentDefinition = $WebhookData }
+else { $deploymentDefinition = $WebhookData.RequestBody }
 
 Write-Verbose $deploymentDefinition
 
